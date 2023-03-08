acea7ce0adde08690766e36037de2a934431f8a5
Mohamed-Echbiy <echbiymed@gmail.com>
2023-03-08 12:50:08 +0100

    *

--- server.js
+++ server.js
@@ -15,56 +15,6 @@ const cache = new NodeCache();
 
 app.listen(PORT, () => console.log("started"));
 
-// app.get("/:chapter", async (req, res) => {
-//   const { chapter } = req.params;
-//   const { ep } = req.query;
-
-//   async function title(chapter) {
-//     const filter = /[^a-zA-Z0-9?!]/g;
-//     const composedTitle = chapter.replace(filter, "-");
-//     return composedTitle.toLowerCase();
-//   }
-//   const titleIs = await title(chapter);
-//   console.log(titleIs);
-//   const url = `https://com.cloud-anime.com/anime/${titleIs}`;
-//   console.log(url, "I am url");
-//   try {
-//     const { data } = await axios.get(url);
-//     // console.log(data);
-//     const $ = Cheerio.load(data);
-//     const links = $(`.episodes-card-title h3 a`)
-//       .get()
-//       .map((val) => $(val).attr("href"));
-//     const { data: getLink } = await axios.get(links[ep - 1]);
-//     const $1 = Cheerio.load(getLink);
-//     const epLinks = $1(`ul.nav-tabs li a[data-ep-url]`).get();
-//     // .map((val) => $1(val).attr("data-ep-url"));
-//     console.log(epLinks, "first return ");
-//     const fetchAnother = async () => {
-//       if (!epLinks.length) {
-//         console.log("the condition has started");
-//         const $c = Cheerio.load(data);
-//         const links = $c(`.episodes-card-title h3 a`)
-//           .get()
-//           .map((val) => $c(val).attr("href"));
-//         const { data: getLink2 } = await axios.get(links[ep]);
-//         const $2 = Cheerio.load(getLink2);
-//         const Adata = $2(`ul.nav-tabs li a`)
-//           .get()
-//           .map((val) => $2(val).attr("data-ep-url"));
-//         return Adata;
-//       }
-//       return epLinks;
-//     };
-
-//     console.log(await fetchAnother());
-//     res.status(200).json({ data: await fetchAnother() });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-//
-
 app.get("/:chapter", async (req, res) => {
   const { chapter } = req.params;
   const { ep } = req.query;
